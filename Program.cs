873c61c92b27b0c7fb8cbc75fc6493e4014cb59c
tantting <jennyann.hayward82@gmail.com>
2024-09-25 11:21:18 +0200

    Changed text

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Nu kör vi! ");
+        Console.WriteLine("Yes");
+        Console.WriteLine("Wohoo");
         
     }
 }
